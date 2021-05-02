162d0d24ecf5b6ff68e51b26cf1aaa28f503c33b
Christian Bedoya <chris_bedoya01@hotmail.com>
2021-05-02 17:26:05 -0500

    hello route and init

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Flask start"""
+from flask import Flask
+import flask
+
+app = flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """Hello"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
