bccbcc7dfc3f8d519d9bff62b1b1967dad8cbf99
christian bedoya <chrisdav1022>
2021-05-03 00:22:23 +0000

    route python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,26 +6,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """main page"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb", methods=["GET"], strict_slashes=False)
 def hbnb():
     """str"""
     return "HBNB"
 
 
-@app.route('/c/<phrase>', strict_slashes=False)
+@app.route('/c/<phrase>', methods=["GET"], strict_slashes=False)
 def c(phrase):
     """Print c phrases"""
     return 'C {}'.format(phrase.replace('_', ' '))
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<phrase>', strict_slashes=False)
+@app.route('/python/', methods=["GET"], strict_slashes=False)
+@app.route('/python/<phrase>', methods=["GET"], strict_slashes=False)
 def python(phrase='is cool'):
     """string and is cool"""
     return 'Python {}'.format(phrase.replace('_', ' '))
